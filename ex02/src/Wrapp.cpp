db66a3369b8199e8246eaf9ed6a400391388413b
oishchen42 <alex.di.master@gmail.com>
2026-04-25 13:44:44 +0200

    add:blueprint of FJ algorithm; TODO: generic functions + Jacobstahl sque function

--- ex02/src/Wrapp.cpp
+++ ex02/src/Wrapp.cpp
@@ -1,19 +1,29 @@
 #include "Wrapp.hpp"
 
-static void printVec(const std::vector<int>& vec)
-{
-    for(const auto& v : vec)
-    {
-        std::cout << v << " ";
-    }
-    std::cout << std::endl;
-}
+// static void printVec(const std::vector<int>& vec)
+// {
+//     for(const auto& v : vec)
+//     {
+//         std::cout << v << " ";
+//     }
+//     std::cout << std::endl;
+// }
 
-static void dqPrint(const std::deque<int>& dqs)
+// static void dqPrint(const std::deque<int>& dqs)
+// {
+//     for(const auto& dq : dqs)
+//     {
+//         std::cout << dq << " ";
+//     }
+//     std::cout << std::endl;
+// }
+
+template <typename T>
+static void pairsPrint(const T& box)
 {
-    for(const auto& dq : dqs)
+    for (const auto& item : box)
     {
-        std::cout << dq << " ";
+        std::cout << "[" << item.first << "; " << item.second << "] ";
     }
     std::cout << std::endl;
 }
@@ -113,44 +123,116 @@ bool isSorted(I first, I last)
     return true;
 }
 
+template <typename T>
+static void pairsSortByFirstElement(T& pairs)
+{
+    if (pairs.size() <= 1)
+        return ;
+    size_t left = pairs.size() / 2;
+    T left_arr(pairs.begin(), pairs.begin() + left);
+    T right_arr(pairs.begin() + left, pairs.end());
+    // std::cout << "Left array: ";
+    // pairsPrint(left_arr);
+    // std::cout << "Right array: ";
+    // pairsPrint(right_arr);
+    pairsSortByFirstElement(left_arr);
+    pairsSortByFirstElement(right_arr);
+    size_t l = 0;
+    size_t r = 0;
+    size_t a = 0;
+    while (l < left_arr.size() && r < right_arr.size())
+    {
+        if (left_arr[l].first <= right_arr[r].first)
+            pairs[v++] = left_arr[l++];
+        else
+            pairs[v++] = right_arr[r++];
+    }
+    while (l < left_arr.size())
+        pairs[v++] = left_arr[l++];
+    while (r < right_arr.size())
+        pairs[v++] = right_arr[r++];
+}
+
+static void sortVecPairs(std::vector<int>& vec)
+{
+    if (vec.size() <= 1)
+        return ;
+    int tail = -1;
+    std::vector<std::pair<int, int>> pairs;
+    if (vec.size() % 2 != 0)
+    {
+        tail = vec.at(vec.size() - 1);
+        vec.pop_back();
+    }
+    pairs.reserve(vec.size() / 2);
+    for (size_t i = 1; i < vec.size(); i+=2)
+    {
+        if (vec.at(i) > vec.at(i - 1))
+            pairs.push_back(std::make_pair(vec.at(i), vec.at(i - 1)));
+        else
+            pairs.push_back(std::make_pair(vec.at(i - 1), vec.at(i)));
+    }
+    // std::cout << "Our pairs: ";
+
+    // pairsPrint(pairs);
+    // std::cout << "===================================\n";
+    pairsSortByFirstElement(pairs);
+    std::vector<int> ft_main;
+    std::vector<int> ft_pend;
+    ft_main.reserve(pairs.size());
+    ft_pend.reserve(pairs.size());
+    dividePairs(pairs, ft_pend, ft_main);
+    ft_main.insert(ft_main.begin(), ft_pend.at(0));
+    std::vector<int> jcb_sque = getSequence(ft_pend.size());
+    insertJcb(jcb_sque, ft_pend, ft_main);
+}
+
 void Wrapp::sortVec(const std::string& input)
 {
-    struct timeval start;
-    struct timeval end;
     checkInput(input);
     std::vector<int> vec;
     recordInput(input, vec);
-    std::cout << "==========Vec before: ============================\n";
-    printVec(vec);
-    std::cout << "IS Container sorted: " << isSorted(vec.begin(), vec.end()) << std::endl;
-    std::cout <<"====================================================\n";
-
-    gettimeofday(&start, NULL);
-    mergeSort(vec);
-    gettimeofday(&end, NULL);
-    double sec = end.tv_sec - start.tv_sec;
-    double msec = end.tv_usec - start.tv_usec;
-
-    std::cout << "==========Vec after: ============================\n";
-    printVec(vec);
-    std::cout << "IS Container sorted: " << isSorted(vec.begin(), vec.end()) << std::endl;
-    std::cout << "Time to process a range of " << vec.size() << " elements with std::vec is: " << ((sec * 1000000) + msec) / 1000000 << " us" << std::endl;
-    std::cout <<"====================================================\n";
-
-    std::deque<int> dq;
-    recordInput(input, dq);
-    std::cout << "==========DQ before: ============================\n";
-    dqPrint(dq);
-    std::cout << "IS Container sorted: " << isSorted(dq.begin(), dq.end()) << std::endl;
-    std::cout <<"====================================================\n";
-    gettimeofday(&start, NULL);
-    mergeSort(dq);
-    gettimeofday(&end, NULL);
-    sec = end.tv_sec - start.tv_sec;
-    msec = end.tv_usec - start.tv_usec;
-    std::cout << "==========DQ after: ============================\n";
-    dqPrint(dq);
-    std::cout << "IS Container sorted: " << isSorted(dq.begin(), dq.end()) << std::endl;
-    std::cout << "Time to process a range of " << dq.size() << " elements with std::deque is: " << ((sec * 1000000) + msec) / 1000000 << " us" << std::endl;
-    std::cout <<"====================================================\n";
+    sortVecPairs(vec);
 }
+
+// void Wrapp::sortVec(const std::string& input)
+// {
+//     struct timeval start;
+//     struct timeval end;
+//     checkInput(input);
+//     std::vector<int> vec;
+//     recordInput(input, vec);
+//     std::cout << "==========Vec before: ============================\n";
+//     printVec(vec);
+//     std::cout << "IS Container sorted: " << isSorted(vec.begin(), vec.end()) << std::endl;
+//     std::cout <<"====================================================\n";
+
+//     gettimeofday(&start, NULL);
+//     mergeSort(vec);
+//     gettimeofday(&end, NULL);
+//     double sec = end.tv_sec - start.tv_sec;
+//     double msec = end.tv_usec - start.tv_usec;
+
+//     std::cout << "==========Vec after: ============================\n";
+//     printVec(vec);
+//     std::cout << "IS Container sorted: " << isSorted(vec.begin(), vec.end()) << std::endl;
+//     std::cout << "Time to process a range of " << vec.size() << " elements with std::vec is: " << ((sec * 1000000) + msec) / 1000000 << " us" << std::endl;
+//     std::cout <<"====================================================\n";
+
+//     std::deque<int> dq;
+//     recordInput(input, dq);
+//     std::cout << "==========DQ before: ============================\n";
+//     dqPrint(dq);
+//     std::cout << "IS Container sorted: " << isSorted(dq.begin(), dq.end()) << std::endl;
+//     std::cout <<"====================================================\n";
+//     gettimeofday(&start, NULL);
+//     mergeSort(dq);
+//     gettimeofday(&end, NULL);
+//     sec = end.tv_sec - start.tv_sec;
+//     msec = end.tv_usec - start.tv_usec;
+//     std::cout << "==========DQ after: ============================\n";
+//     dqPrint(dq);
+//     std::cout << "IS Container sorted: " << isSorted(dq.begin(), dq.end()) << std::endl;
+//     std::cout << "Time to process a range of " << dq.size() << " elements with std::deque is: " << ((sec * 1000000) + msec) / 1000000 << " us" << std::endl;
+//     std::cout <<"====================================================\n";
+// }
